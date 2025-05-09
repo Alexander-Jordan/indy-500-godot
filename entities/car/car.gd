cfe3b5b453c26868cae046f1fa486be180c2d234
Alexander-Jordan <curtjordan93@gmail.com>
2025-05-09 12:15:13 +0200

    Implement a way of calculating top speed to get max slip speed, and simplify all the code a bit everywhere

--- entities/car/car.gd
+++ entities/car/car.gd
@@ -1,35 +1,31 @@
 class_name Car extends CharacterBody2D
 
-@export var drag: float = 0.12
+@export_range(0, 1, 0.0001) var drag: float = 0.0020
 @export var engine_power: float = 500.0
-@export var friction: float = 55
+@export_range(0, 1, 0.01) var friction: float = 0.9
 @export_enum('p1', 'p2') var player: String = 'p1'
-@export var slip_speed: float = 200
 @export var speed_reverse_max: float = 250.0
 ## Amount that front wheel turns, in degrees
 @export var steering_angle_degrees: float = 15
-@export var traction_fast: float = 2.5
-@export var traction_slow: float = 10.0
+@export var traction_min: float = 1.0
+@export var traction_max: float = 10.0
 ## Distance from front to rear wheel.
 @export var wheel_base: float = 10
 
-var acceleration: Vector2 = Vector2.ZERO
+var speed_min: float = 20
+var speed_max: float = get_max_speed()
 var steering_direction: float = 0.0
 
 func _physics_process(delta: float) -> void:
-	drag_and_friction(delta)
-	calculate_steering(delta)
-	velocity += acceleration * delta
-	move_and_slide()
-
-func _process(delta: float) -> void:
-	accelerate()
+	var acceleration: Vector2 = get_acceleration()
 	steer()
-
-func accelerate() -> void:
-	acceleration = Vector2.ZERO
-	var input = -Input.get_axis(player + '_up', player + '_down')
-	acceleration = transform.x * (engine_power * input)
+	calculate_steering(delta)
+	
+	if acceleration.length() < speed_min and velocity.length() < speed_min:
+		velocity = Vector2.ZERO
+	velocity += acceleration * delta
+	
+	move_and_slide()
 
 func calculate_steering(delta: float) -> void:
 	# Find the wheel positions
@@ -51,22 +47,35 @@ func calculate_steering(delta: float) -> void:
 	# Set the rotation to the new direction
 	rotation = new_heading.angle()
 
-func drag_and_friction(delta: float) -> void:
-	if acceleration == Vector2.ZERO and velocity.length() < 50:
-		velocity = Vector2.ZERO
-	var friction_force: Vector2 = -velocity * friction * delta
-	var drag_force: Vector2 = -velocity * velocity.length() * drag * delta
-	acceleration += drag_force + friction_force
+func get_acceleration() -> Vector2:
+	var acceleration: Vector2 = Vector2.ZERO
+	var input = -Input.get_axis(player + '_up', player + '_down')
+	acceleration = transform.x * (engine_power * input)
+	acceleration -= get_drag_force() + get_friction_force()
+	return acceleration
+
+func get_drag_force() -> Vector2:
+	return velocity * velocity.length() * drag
+
+func get_friction_force() -> Vector2:
+	return velocity * friction
+
+## Calculate and get the max speed from drag, friction, and engine_power.
+## It's solving the quadratic equation (a*x^2) + (b*x) - c = 0
+## a = drag, b = friction, c = engine_power, and x = velocity
+## It solves it using the quadratic formula
+## (-b + sqrt((b*b) + (4*a*c))) / (2*a)
+func get_max_speed() -> float:
+	var a: float = drag
+	var b: float = friction
+	var c: float = engine_power
+	var discriminant = sqrt((b * b) + (4.0 * a * c))
+	var velocity_magnitude_max = (-b + discriminant) / (2.0 * a)
+	return velocity_magnitude_max
 
 func get_traction() -> float:
-	var slip_speed_min: float = 50
-	var slip_speed_max: float = 320
-	var traction_min: float = 1.0
-	var traction_max: float = 10.0
-	
-	var normalized_slip: float = (velocity.length() - slip_speed_min) / (slip_speed_max - slip_speed_min)
+	var normalized_slip: float = (velocity.length() - speed_min) / (speed_max - speed_min)
 	normalized_slip = clampf(normalized_slip, 0.0, 1.0)
-	
 	return lerpf(traction_max, traction_min, normalized_slip)
 
 func steer() -> void:
